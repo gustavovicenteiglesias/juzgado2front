12ea4868b82b14bbd320514d98827cb0da9b32e2
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2021-09-29 20:58:12 -0300

    juzgadopre entrega1

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/"//"http://areco.gob.ar:9528/api/auth/";
+const API_URL = "http://localhost:9531/api/auth/"//"http://areco.gob.ar:9528/api/auth/";
 
 class AuthService {
   login(username, password) {
